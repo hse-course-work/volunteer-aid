7bfbd703724ae55892691ad2115b7ef654d12300
kolpakovee <eekolpakov@edu.hse.ru>
2024-01-03 10:51:22 +0300

    remove nginx container

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -37,7 +37,7 @@ object Main extends zio.ZIOAppDefault {
     ZIO.executor.flatMap(executor =>
       BlazeServerBuilder[Task]
         .withExecutionContext(executor.asExecutionContext)
-        .bindHttp(8080, "localhost")
+        .bindHttp(8080, "0.0.0.0")
         .withHttpApp(Router("/" -> (routes <+> swaggerRoutes(mainRouter.getUser))).orNotFound)
         .serve
         .compile
